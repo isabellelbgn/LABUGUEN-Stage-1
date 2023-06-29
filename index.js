91e367987799cea97cfc4c2082b11782b2b38477
isabellelbgn <116334027+isabellelbgn@users.noreply.github.com>
2023-06-29 18:49:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const serverPort = 3000;
 const server = http.createServer((request, response) => {
   response.statusCode = 200;
   response.setHeader("Content-Type", "text/plain");
-  response.end("JavaScript");
+  response.send("JavaScript");
 });
 
 server.listen(serverPort, () => {
